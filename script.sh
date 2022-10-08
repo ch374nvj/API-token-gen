bee7cf16ea554c273fc3ad848fba807ac31da6e6
Chetan <cheta@pop-os.localdomain>
2022-10-08 18:20:46 +0530

    lil more modifications

--- script.sh
+++ script.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-#echo enter the client id: 
-
 read -p 'enter client id: ' clientid
-
 echo  client id is $clientid
-
-#echo enter client secret: 
-
 read -s 'enter client secret: ' clientsec
 
-#echo client secret is $clientsec
-
-#echo enter auth url
-
 read -p 'enter auth uri: ' authl
-
-#echo enter scope
-
 read -p 'enter scope: ' scope
 
 echo $authl?scope\=$scope\&response_type\=code\&access_type\=offline\&redirect_uri\=https\://localhost\&client_id\=$clientid
